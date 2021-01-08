dd23d46095afbe5a261786beadb33c3d7da98c53
Anthony <e0406854@u.nus.edu>
2021-01-08 19:10:25 +0800

    feat(Authentication): register, login and sign out

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:hacknroll2021/pages/home_page.dart';
-import 'package:hacknroll2021/pages/Onboarding/slider.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_core/firebase_core.dart';
+import 'package:hacknroll2021/services/auth.dart';
 import 'pages/login_page.dart';
+import 'package:provider/provider.dart';
+import './models/user.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      // Initialize FlutterFire:
-      future: _initialization,
-      builder: (context, snapshot) {
-        // Check for errors
-        // if (snapshot.hasError) {
-        //   return Scaffold(
-        //
-        //   );
-        // }
-
-        // Once complete, show your application
-        if (snapshot.connectionState == ConnectionState.done) {
-          return MaterialApp(
-            title: 'CHAKmate2021',
-            debugShowCheckedModeBanner: false,
-            theme: ThemeData(
-              primarySwatch: Colors.blue,
-              visualDensity: VisualDensity.adaptivePlatformDensity,
-            ),
-            initialRoute: '/',
-            routes: {
-              '/': (context) => LoginPage(),
-
-            },
-          );
-        }
-
-        // Otherwise, show something whilst waiting for initialization to complete
-        // return Scaffold(
-        //
-        // );;
-        return Scaffold();
-      },
+    // Once complete, show your application
+    return StreamProvider<CurrentUser>.value(
+      value: Auth().user,
+      child: MaterialApp(
+        title: 'CHAKmate2021',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => LoginPage(),
+        },
+      ),
     );
   }
 }
