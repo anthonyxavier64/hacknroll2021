605de3c8ad46f13a133332d2ea9aa9d10bc491e1
ccwhgetgit <65432561+ccwhgetgit@users.noreply.github.com>
2021-01-09 03:16:14 +0800

    Update home_page.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -21,6 +21,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+          automaticallyImplyLeading: false,
           title: Text('List of Faculties'),
           actions: [
             FlatButton(
