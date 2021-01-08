5db1865244c6f848cf12ee63661563b336319aba
kieron560 <kieron560@gmail.com>
2021-01-08 17:37:13 +0800

    attempted Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:hacknroll2021/pages/home_page.dart';
+import 'package:hacknroll2021/pages/Onboarding/slider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'pages/login_page.dart';
 
 void main() {
@@ -7,19 +10,43 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CHAKmate2021',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => LoginPage(),
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        // if (snapshot.hasError) {
+        //   return Scaffold(
+        //
+        //   );
+        // }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MaterialApp(
+            title: 'CHAKmate2021',
+            debugShowCheckedModeBanner: false,
+            theme: ThemeData(
+              primarySwatch: Colors.blue,
+              visualDensity: VisualDensity.adaptivePlatformDensity,
+            ),
+            initialRoute: '/',
+            routes: {
+              '/': (context) => LoginPage(),
+
+            },
+          );
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        // return Scaffold(
+        //
+        // );;
+        return Scaffold();
       },
     );
   }
--- lib/pages/Onboarding/slide_item.dart
+++ lib/pages/Onboarding/slide_item.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:Cycled_iOS/onboarding/slider.dart';
+import '../onboarding/slider.dart';
 
 class SlideItem extends StatelessWidget {
   final int index;
