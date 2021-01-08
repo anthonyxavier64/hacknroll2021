44f97bff3df3e31beeeb5af186deba62e0f80c4f
Anthony <e0406854@u.nus.edu>
2021-01-08 13:31:49 +0800

    changed hompage.dart to login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        title: Text(''),
+      ),
     );
   }
 }
